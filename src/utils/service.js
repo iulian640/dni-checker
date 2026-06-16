6f9e3e43c6b49cb1b4d780d880765a2739628890
iulian640 <iuliantim21@gmail.com>
2026-06-16 11:21:11 +0200

    feat: function structure created

---
+++ src/utils/service.js
@@ -0,0 +1,6 @@
+const dni = 99999999
+const letraRaw = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
+function checker(dni) {
+    let resto = dni % 23
+
+}
