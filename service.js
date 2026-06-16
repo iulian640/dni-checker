a185280100dd75b741e46ff3091f784625fd5330
iulian640 <iuliantim21@gmail.com>
2026-06-16 14:26:05 +0200

    refactor: vue deleted

---
+++ service.js
@@ -0,0 +1,10 @@
+
+
+function checker(dni) {
+
+    const letraRaw = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
+    let resto = dni % 23
+    let letra = letraRaw[resto]
+    return letra
+}
+console.log(checker(99999999))
