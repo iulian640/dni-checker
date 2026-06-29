745e56cc09d0589d5d9fe0f65c5a0213b248ab66
iulian640 <iuliantim21@gmail.com>
2026-06-29 13:29:27 +0200

    feat: add mostrarFormulario and añadirAlHistorial to rendering

--- src/rendering.js
+++ src/rendering.js
@@ -1,4 +1,15 @@
-import { createButton } from './htmlcomponents.js'
+import { createButton, createForm } from './htmlcomponents.js'
 
 const boton = createButton()
 document.body.appendChild(boton)
+
+export function mostrarFormulario() {
+    document.getElementById('iniciar').remove()
+    document.body.appendChild(createForm())
+}
+
+export function añadirAlHistorial(texto) {
+    const li = document.createElement('li')
+    li.textContent = texto
+    document.getElementById('historial').appendChild(li)
+}
